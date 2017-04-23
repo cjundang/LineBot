90acb475141aa8bf65a43debc1b06070cf337545
Chanankorn Jandaeng <cjundang@gmail.com>
2017-04-23 17:12:49 +0700

    add index.php 2

--- index.php
+++ index.php
@@ -19,8 +19,15 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "I don't understand";
 			];
+
+			if($text == "Hi"){
+				$messages = [
+					'type' => 'text',
+					'text' => "Hello";
+				];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
