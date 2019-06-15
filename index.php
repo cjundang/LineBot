0f8a1ab57b6033099d4c70436976e8c1e3135cce
Chanankorn Jandaeng <cjundang@gmail.com>
2019-06-15 17:02:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,5 @@
 <?php
-$access_token = 'hlr7fbARgRCAHHddkqHGJyRDCrAX3btnsCOFnEoE+PFXN7Ugul6aY02qn7/kd7nwkusmr5m2CMcQ5KH1P7/2Gjo3/nucRua0C29CnJinuwy4cx6OyOlYyI/7feEOsaC7yl+zu0XZNyuW+T2VKYvvQgdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+ http_response_code(200);
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => "I don't understand"
-			];
-
-			if($text == "Hi"){
-				$messages = [
-					'type' => 'text',
-					'text' => "Hello"
-				];
-			}
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-echo "OK";
+?>
